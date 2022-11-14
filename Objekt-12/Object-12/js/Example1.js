7a6c107f52df6588b24d1e5eeda44b43adb6a20f
Nurdan Ozkan <117853743+nurdan-ozkan@users.noreply.github.com>
2022-11-14 17:12:45 -0600

    Delete Objekt-12 directory

--- Objekt-12/Object-12/js/Example1.js
+++
@@ -1,7 +0,0 @@
-const meyve=["elma", "armut", 45, 67, "cilek",23];
-let t="";
-for(let i=0; i<meyve.length; i++){
-    t+=meyve[i]+"<br>"
-}
-
-document.getElementById("demo1").innerHTML=t;
--- Objekt-12/js/Examlple2.js
+++
@@ -1,8 +0,0 @@
-let araba={
-    type: "BMW", 
-    model: "2345",
-    color: "yesil",
-    hiz: 200
-}
-
-document.getElementById("demo2").innerHTML=araba.type + araba.hiz;
