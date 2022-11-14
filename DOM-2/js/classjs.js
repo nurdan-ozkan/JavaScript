29a4788f18c1287b579662a21912e8331efeefd8
Nurdan Ozkan <117853743+nurdan-ozkan@users.noreply.github.com>
2022-11-14 17:00:38 -0600

    Add files via upload

---
+++ DOM-2/js/classjs.js
@@ -0,0 +1,4 @@
+function CName(){
+    let x=document.getElementsByClassName("demo1");
+    x[2].innerHTML="Hello";
+}
---
+++ DOM-2/js/query.js
@@ -0,0 +1,5 @@
+function  QDeneme(){
+    let t=document.querySelectorAll("h1.d");
+    t [1].innerHTML="Result";
+    t [1].style.background="blue";
+}
