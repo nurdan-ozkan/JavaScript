a495de1a84c8090f3aef2ea03e7047ed4364fbf7
Nurdan Ozkan <117853743+nurdan-ozkan@users.noreply.github.com>
2022-11-14 17:03:58 -0600

    Add files via upload

---
+++ Window-13/js/Example1.js
@@ -0,0 +1,11 @@
+let pencere;
+function SayfaAc(){
+    pencere=window.open(" ", " ", " width=500 , height=500" );
+
+}
+
+ 
+function SayfaKapat(){
+    pencere.close();
+
+}
---
+++ Window-13/js/Example2.js
@@ -0,0 +1,9 @@
+let d;
+
+function Sayfakapat(){
+d.close();
+}
+
+function Sayfaac(){
+    d=window.open("https://aws.amazon.com/free/?trk=fce796e8-4ceb-48e0-9767-89f7873fac3d&sc_channel=ps&sc_campaign=acquisition&sc_medium=ACQ-P|PS-GO|Brand|Desktop|SU|Core-Main|Core|US|EN|Text&s_kwcid=AL!4422!3!592542020599!e!!g!!aws&ef_id=Cj0KCQjwxveXBhDDARIsAI0Q0x35KWEtZTTOlvvdOukCj7bGD3TDwNNl80sNEmdny0NxZUqDsoswBVUaAt9NEALw_wcB:G:s&s_kwcid=AL!4422!3!592542020599!e!!g!!aws&all-free-tier.sort-by=item.additionalFields.SortRank&all-free-tier.sort-order=asc&awsf.Free%20Tier%20Types=*all&awsf.Free%20Tier%20Categories=*al" ,"_blank", "width=300,height=300")
+}
